e9c11d8dc7f25d91552c3d5018fc8321c8c56c0d
head47 <63517545+head47@users.noreply.github.com>
2022-03-28 22:33:49 +0300

    implement HCS

--- caesar.py
+++ caesar.py
@@ -1,4 +1,23 @@
 import idaapi
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def highly_connected(G,E):
+    return len(E) > len(G.nodes) / 2
+
+def HCS(G_):
+    G = G_.copy()
+    if len(G.nodes) == 1:
+        return G
+    E = nx.algorithms.connectivity.cuts.minimum_edge_cut(G)
+    if not highly_connected(G,E):
+        G.remove_edges_from(E)
+        print('removing',E)
+        subgraphs = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+        subgraphs[0] = HCS(subgraphs[0])
+        subgraphs[1] = HCS(subgraphs[1])
+        G = nx.compose(subgraphs[0],subgraphs[1])
+    return G
 
 class myplugin_t(idaapi.plugin_t):
     flags = idaapi.PLUGIN_UNL
@@ -38,8 +57,17 @@ class myplugin_t(idaapi.plugin_t):
                     curAddr += insnLen
             i += 1
 
-        print(funcs)
-        print(connections)
+        funcG = nx.Graph()
+        funcG.add_edges_from(connections)
+        dividedG = HCS(funcG)
+
+        subax1 = plt.subplot(121)
+        nx.draw(funcG, with_labels=True)
+        subax2 = plt.subplot(122)
+        nx.draw(dividedG, with_labels=True)
+        plt.show()
+        print(funcG.nodes)
+        print(funcG.edges)
 
     def term(self):
         pass
